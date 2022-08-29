99914cd4626a6acd48a3d4f01f26f3173aa9bf62
0xTovand <tovand@protonmail.com>
2022-08-29 14:44:12 +0300

    search issues form

--- src/components/IssuesList.tsx
+++ src/components/IssuesList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { IssueItem } from './IssueItem';
 
@@ -9,6 +9,7 @@ export default function IssuesList({
   labels: string[];
   status: string;
 }) {
+  const [searchValue, setSearchValue] = useState('');
   const issuesQuery = useQuery(['issues', { labels, status }], () => {
     const statusString = status ? `&status=${status}` : '';
     const labelsString = labels.map((label) => `labels[]=${label}`).join('&');
@@ -16,17 +17,62 @@ export default function IssuesList({
       res.json()
     );
   });
+
+  const searchQuery = useQuery(
+    ['issues', 'search', searchValue],
+    () =>
+      fetch(`/api/search/issues?q=${searchValue}`).then((res) => res.json()),
+    {
+      enabled: searchValue.length > 0,
+    }
+  );
+
   return (
     <div>
+      <form
+        onSubmit={(event: any) => {
+          event.preventDefault();
+          setSearchValue(event.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search">Search Issues</label>
+        <input
+          type="search"
+          placeholder="Search"
+          name="search"
+          id="search"
+          onChange={(event) => {
+            if (event.target.value.length === 0) {
+              setSearchValue('');
+            }
+          }}
+        />
+      </form>
       <h2>Issues List</h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
-      ) : (
+      ) : searchQuery.fetchStatus === 'idle' && searchQuery.isLoading ? (
         <ul className="issues-list">
           {issuesQuery.data.map((issue) => (
             <IssueItem key={issue.id} {...issue} />
           ))}
         </ul>
+      ) : (
+        <>
+          <h2>Search results</h2>
+          {searchQuery.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <p>{searchQuery.data.count} Results</p>
+              <ul className="issues-list">
+                {searchQuery.data.items.map((issue) => (
+                  <IssueItem key={issue.id} {...issue} />
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       )}
     </div>
   );
--- src/helpers/useUserData.ts
+++ src/helpers/useUserData.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 export function useUserData(userId: string) {
+  if (!userId) return null;
   const userData = useQuery(['users', userId], () =>
     fetch(`/api/users/${userId}`).then((res) => res.json())
   );
